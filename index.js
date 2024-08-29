06c5b354e1f33820befe358eafc19439e9a354ef
Lucas Rodrigues <lucasrodrigues@unifucamp.edu.br>
2024-08-29 00:44:37 -0300

    Novas funcionalidades

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const titleElement = document.title;
-    const text = "Code";
+    const text = "LukeCode";
+    const defaultTitle = "Portfólio";
     let index = 0;
     let currentTitle = '';
+    let isErasing = false;
 
     function typeTitle() {
-        if (index < text.length) {
-            currentTitle += text.charAt(index);
+        if (!isErasing) {
+            if (index < text.length) {
+                currentTitle += text.charAt(index);
+                document.title = currentTitle;
+                index++;
+                setTimeout(typeTitle, 150); // Ajuste a velocidade da digitação
+            } else {
+                isErasing = true;
+                setTimeout(eraseTitle, 2000); // Tempo antes de começar o apagamento
+            }
+        }
+    }
+
+    function eraseTitle() {
+        if (currentTitle.length > 0) {
+            currentTitle = currentTitle.slice(0, -1);
             document.title = currentTitle;
-            index++;
-            setTimeout(typeTitle, 100); 
+            setTimeout(eraseTitle, 100); // Ajuste a velocidade do apagamento
         } else {
+            // Define um título padrão brevemente antes de reiniciar a digitação
+            document.title = defaultTitle;
             setTimeout(() => {
-                currentTitle = '';
                 index = 0;
-                function eraseTitle() {
-                    if (index < text.length) {
-                        currentTitle = currentTitle.slice(0, -1);
-                        document.title = currentTitle;
-                        index++;
-                        setTimeout(eraseTitle, 50);
-                    } else {
-                        setTimeout(typeTitle, 1000); 
-                    }
-                }
-                eraseTitle();
-            }, 2000);
+                isErasing = false;
+                setTimeout(typeTitle, 500); // Tempo de pausa antes de recomeçar a digitação
+            }, 1000); // Tempo do título padrão
         }
     }
 
